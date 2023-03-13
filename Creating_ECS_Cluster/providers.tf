060ee38169d8666dba6d4357f4d8dcb9e30a46f4
Yogendra H Jagadeesh <yogendrahj@gmail.com>
2023-03-13 16:42:52 +0000

    adding ecs creation project

---
+++ Creating_ECS_Cluster/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.20.0"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.58.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+provider "aws" {
+  region = "eu-west-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
