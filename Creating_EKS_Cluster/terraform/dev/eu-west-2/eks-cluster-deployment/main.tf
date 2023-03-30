975fc2ea5af7d3c56b0121034c788f76a29c56b1
Yogendra H Jagadeesh <yogendrahj@gmail.com>
2023-03-30 12:05:31 +0100

    adding main.tf instructions

---
+++ Creating_EKS_Cluster/terraform/dev/eu-west-2/eks-cluster-deployment/main.tf
@@ -0,0 +1,64 @@
+# --- root/main.tf ---
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.60.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = local.location
+}
+
+
+locals {
+  cwd         = reverse(split("/", path.cwd))
+  name        = local.cwd[0]
+  location    = local.cwd[1]
+  environment = local.cwd[2]
+  vpc_cidr    = "10.123.0.0/16"
+
+  tags = {
+    project     = "eks_project"
+    environment = "dev"
+    managedby   = "terraform"
+    owner       = "Yogendra Jagadeesh"
+  }
+}
+
+
+module "networking" {
+  source = "D:/Terraform/terraform_hands_on/Creating_EKS_Cluster/modules/networking"
+  vpc_cidr = local.vpc_cidr
+  tags             = local.tags
+  public_sn_count  = 2
+  private_sn_count = 2
+  availabilityzone = "ueu-west-2a"
+  azs              = 2
+}
+
+
+module "cluster" {
+  source          = "D:/Terraform/terraform_hands_on/Creating_EKS_Cluster/modules/cluster"
+  tags            = local.tags
+  name            = local.name
+  vpc_id          = module.networking.vpc_id
+  ec2_ssh_key     = "EKS-Key"
+  desired_size    = 2
+  min_size        = 1
+  max_size        = 5
+  public_subnets  = module.networking.public_subnets
+  private_subnets = module.networking.private_subnets
+}
+
+
+
+
+
+
+
+
+
